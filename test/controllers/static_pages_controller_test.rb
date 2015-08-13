530a1a7ca19955b5ec42382d13c646b9d4bedeab
Preston <pedmands@gmail.com>
2015-08-13 23:23:03 +0000

    Change a lot of styles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,24 +4,24 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "microPOST by Preston Edmands"
+    assert_select "title", "Chirply by Preston Edmands"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | microPOST by Preston Edmands"
+    assert_select "title", "Help | Chirply by Preston Edmands"
   end
   
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | microPOST by Preston Edmands"
+    assert_select "title", "About | Chirply by Preston Edmands"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | microPOST by Preston Edmands"
+    assert_select "title", "Contact | Chirply by Preston Edmands"
   end
 end
